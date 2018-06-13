45364225e10b283b086b52ca9dd1855d1acd87a5
estudiante <estudiante@uca-ThinkCentre-M710q>
2018-06-13 06:11:40 -0600

    Labo penultimo

---
+++ src/vista/Consulta.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Consulta extends JFrame {
+    public JLabel lblCodigo, lblMarca, lblStock, lblExistencia;
+    
+    public JTextField codigo, descripcion, stock;
+    public JComboBox marca;
+    
+    ButtonGroup existencia = new ButtonGroup();
+}
