b61e9d65fb516c352d21aedd0ba310b6f3b0ed71
LN710Q <LN710Q@DEILABO1-PC22>
2018-06-13 12:38:29 -0600

    Se crearon las clases y paquetes a usar

--- src/inventario2/Inventario2.java
+++ src/inventario2/Inventario2.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package inventario;
+package inventario2;
 
 /**
  *
  * @author estudiante
  */
-public class Inventario {
+public class Inventario2 {
 
     /**
      * @param args the command line arguments
